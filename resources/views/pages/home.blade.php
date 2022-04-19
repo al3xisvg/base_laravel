8ecb3551e47150ae972269dfddf3c031dab75095
al3xisvg <vasquezgarciaalexis@gmail.com>
2022-04-19 15:55:35 -0500

    [uopdate] adding template

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.template')
+
+@section('title', 'Zinout Culture')
+
+@section('content')
+  <h2>Bienvenido a ZC - Zinout Culture</h2>
+
+  <div class="">
+    <h1>asdadasdas</h1>
+  </div>
+@endsection
