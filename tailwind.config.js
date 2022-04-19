6be81d8e35f1182e1a91c90ed30f28478a767646
al3xisvg <vasquezgarciaalexis@gmail.com>
2022-04-18 23:36:00 -0500

    [udpate] adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui')
+  ],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: "dark",
+  },
+}
