a8da61f8b9870942201206a9c82a432c1488c219
al3xisvg <vasquezgarciaalexis@gmail.com>
2022-04-20 11:54:11 -0500

    [update] first endpoint in api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,31 @@ use App\Models\Wp_user;
 use Illuminate\Http\Request;
 
 class UserController extends Controller {
-  public function list() {
-    $users = Wp_user::orderBy('display_name', 'asc')->skip(1)->limit(2)->get(['ID', 'display_name']);
+  public function list(Request $request) {
+    $skip = isset($request->skip) ? $request->skip : 0;
+    $limit = isset($request->limit) ? $request->limit : 10;
+    $orderBy = $request->orderBy;
+    $sort = isset($request->sort) ? $request->sort : 'asc';
+    $fields = isset($request->fields)
+      ? is_array($request->fields)
+        ? $request->fields
+        : []
+      : [];
+
+    $users = [];
+    if (isset($orderBy)) {
+      if (count($fields) > 0) {
+        $users = Wp_user::orderBy($orderBy, $sort)->skip($skip)->limit($limit)->get($fields);
+      } else {
+        $users = Wp_user::orderBy($orderBy, $sort)->skip($skip)->limit($limit)->get();
+      }
+    } else {
+      if (count($fields) > 0) {
+        $users = Wp_user::all()->skip($skip)->limit($limit)->get($fields);
+      } else {
+        $users = Wp_user::all()->skip($skip)->limit($limit)->get();
+      }
+    }
     return $users;
   }
 
