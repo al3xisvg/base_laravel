4b073f4212ad575c27e1e2f4c397a5697bee4455
al3xisvg <vasquezgarciaalexis@gmail.com>
2022-04-24 23:26:08 -0500

    [update] card for add new item

--- resources/views/pages/other.blade.php
+++ resources/views/pages/other.blade.php
@@ -7,19 +7,32 @@
 
   <div class="flex gap-5">
     @foreach ($admins as $admin)
-      <div key="{{ $admin->id }}" class="card w-80 bg-base-100 shadow-xl">
-        <figure class="px-10 pt-10">
-          <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" class="rounded-xl" />
-        </figure>
-        <div class="card-body items-center text-center">
-          <h2 class="card-title">{{ $admin->username }}</h2>
-          <p>{{ $admin->email }}</p>
-          <div class="card-actions">
-            <button id="btnUpd__{{ $admin->id }}" class="btn btn-primary">Editar</button>
-            <button id="btnDel__{{ $admin->id }}" class="btn btn-primary">Eliminar</button>
+      @if ($admin->id === 0)
+        <div key="{{ $admin->id }}" class="card w-80 bg-base-100 shadow-xl cursor-pointer">
+          <figure class="px-10 pt-10">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-28 w-28" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+            </svg>
+          </figure>
+          <div class="card-body items-center text-center">
+            <h1>Agregar Nuevo Admin</h1>
           </div>
         </div>
-      </div>
+      @else
+        <div key="{{ $admin->id }}" class="card w-80 bg-base-100 shadow-xl">
+          <figure class="px-10 pt-10">
+            <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" class="rounded-xl" />
+          </figure>
+          <div class="card-body items-center text-center">
+            <h2 class="card-title">{{ $admin->username }}</h2>
+            <p>{{ $admin->email }}</p>
+            <div class="card-actions">
+              <button id="btnUpd__{{ $admin->id }}" class="btn btn-primary">Editar</button>
+              <button id="btnDel__{{ $admin->id }}" class="btn btn-primary">Eliminar</button>
+            </div>
+          </div>
+        </div>
+      @endif
     @endforeach
   </div>
 
